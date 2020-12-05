73cf8f50816a2d8807e04cd876fdee979ac19eae
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-12-05 00:42:56 -0300

    Corrige execução do programa via .exe no Windows

--- Program.cs
+++ Program.cs
@@ -18,6 +18,9 @@ namespace AulaVersaoOS
             Console.WriteLine(FiggleFonts.Big.Render($"{nomeSO}"));
             Console.WriteLine(FiggleFonts.Big.Render($"{arquiteturaSO}"));
             Console.ResetColor();
+
+            Console.WriteLine("\n\nPressione uma tecla para finalizar...");
+            Console.ReadKey();
         }
     }
 }
