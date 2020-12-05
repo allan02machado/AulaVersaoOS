065a230ecec4fa21d1a06c543a11976b0f811c51
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-12-04 23:50:30 -0300

    Finaliza o código fonte do projeto

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Figgle;
+
+namespace AulaVersaoOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeSO = System.Runtime.InteropServices.RuntimeInformation.OSDescription;
+            string arquiteturaSO = System.Runtime.InteropServices.RuntimeInformation.OSArchitecture.
+                ToString();            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Descubra qual o seu Sistema Operacional");
+            Console.WriteLine("Pressione uma tecla para prosseguir...");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(FiggleFonts.Big.Render($"{nomeSO}"));
+            Console.WriteLine(FiggleFonts.Big.Render($"{arquiteturaSO}"));
+            Console.ResetColor();
+        }
+    }
+}
